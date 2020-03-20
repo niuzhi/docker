50c3cebbf5c18ade794265b60e99b5f7cdb1bf34
Will Ezell <will@dotcms.com>
2020-03-20 13:21:00 -0400

    pulling reference implementation down at runtime

--- images/dotcms-demo/ROOT/srv/dotcms-demo.sh
+++ images/dotcms-demo/ROOT/srv/dotcms-demo.sh
@@ -5,5 +5,6 @@ echo "running docker"
 /usr/bin/dockerd &> /dockerd.log & 
 sleep 5
 cd /dotcms
+wget -q https://raw.githubusercontent.com/dotCMS/docker/master/referenceImplementations/02-single_node/docker-compose.yml
 docker-compose up
 echo "done!"
